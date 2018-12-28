4d49a9df5e0495b96b3caaca4b12aa5cd576e32d
cutiko <cutiko@gmail.com>
2018-12-28 20:54:15 -0300

    ADD: keys for extras

--- shared/src/main/java/cl/cutiko/shared/navigation/FeatureC.java
+++ shared/src/main/java/cl/cutiko/shared/navigation/FeatureC.java
@@ -5,5 +5,7 @@ import cl.cutiko.shared.BuildConfig;
 public class FeatureC {
 
     public static final String ACTION = BuildConfig.FeatureCActivity;
+    public static final String STRING_KEY_EXAMPLE = "cl.cutiko.featurec.FeatureCActivity.key.STRING_KEY_EXAMPLE";
+    public static final String STRING_SERIALIZABLE_EXAMPLE = "cl.cutiko.featurec.FeatureCActivity.key.STRING_SERIALIZABLE_EXAMPLE";
 
 }
